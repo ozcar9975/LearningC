3e670a934293fa232954574a4449cef5bd18a001
oscar <oscar.zhang9812@gmail.com>
2024-12-12 20:58:22 -0500

    learned comments

---
+++ commenting.c
@@ -0,0 +1,27 @@
+//
+// Created by Oscar Zhang on 12/12/24.
+//
+
+#include <stdio.h>
+
+int main() {
+
+    // This is a comment blah
+
+    /*
+     * This
+     * is
+     * a
+     * multiline
+     * comment
+     */
+
+    // hmm this is weird, but i think the ide is adding *
+    // NOTE: don't need * for multiline comments
+
+    // basically just like java
+
+    printf("GAY!\n");
+
+    return 0;
+}
