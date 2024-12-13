4195baf1fd622c66efdfc395a0418201b5b55422
oscar <oscar.zhang9812@gmail.com>
2024-12-12 21:07:43 -0500

    learned common escape sequences

---
+++ escape_seq.c
@@ -0,0 +1,18 @@
+//
+// Created by Oscar Zhang on 12/12/24.
+//
+
+#include <stdio.h>
+
+int main() {
+
+    // common escape sequences
+    // \n - new line - \n
+    // \t - tab
+    // \" \' \\ - common escape sequences for ",', and \
+
+    printf("1\t2\t3\n4\t5\t6\n7\t8\t9\n\n");
+    printf("\"I really love pizza!\" - Oscar probably");
+
+    return 0;
+}
