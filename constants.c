b5c50d50dfa5522ed2afb5b72344f53d9e2d45ef
oscar <oscar.zhang9812@gmail.com>
2024-12-15 19:37:19 -0500

    learned constants

---
+++ constants.c
@@ -0,0 +1,16 @@
+//
+// Created by Oscar Zhang on 12/15/24.
+//
+
+#include <stdio.h>
+
+int main() {
+
+    // constant = fixed value that cannot be altered by the program during its execution
+
+    const float PI = 3.14159;
+
+    printf("%f", PI);
+
+    return 0;
+}
