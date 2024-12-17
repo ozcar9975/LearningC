b9fd2e3aaf8be5ee5f210ccb1336c23deb418a0e
oscar <oscar.zhang9812@gmail.com>
2024-12-16 19:49:02 -0500

    learned basic scanf with ints

--- lesson files/user_input.c
+++ lesson files/user_input.c
@@ -1,3 +1,18 @@
 //
 // Created by Oscar Zhang on 12/16/24.
 //
+
+#include <stdio.h>
+
+int main() {
+
+    int age;
+
+    printf("\nHow old are you? ");
+    scanf("%d", &age);
+
+    printf("You are %d years old", age);
+
+    return 0;
+
+}
