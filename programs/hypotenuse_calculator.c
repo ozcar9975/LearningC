f943c201c35ea54225faa53c8d8b9e140987bc27
oscar <oscar.zhang9812@gmail.com>
2024-12-17 20:44:05 -0500

    completed hypotenuse calculator program

--- programs/hypotenuse_calculator.c
+++ programs/hypotenuse_calculator.c
@@ -3,7 +3,23 @@
 //
 
 #include <stdio.h>
+#include <math.h>
 
-int maiN() {
+int main() {
 
+    double A;
+    double B;
+    double C;
+
+    printf("What is the length of side A?\n");
+    scanf("%lf", &A);
+
+    printf("What is the length of side B?\n");
+    scanf("%lf", &B);
+
+    C = sqrt(pow(A, 2) + pow(B, 2));
+
+    printf("The length of side C is %.5lf\n", C);
+
+    return 0;
 }
