a53629dbbe6c61eddedca653cae1c8c60572ec56
oscar <oscar.zhang9812@gmail.com>
2024-12-19 01:42:17 -0500

    finished temperature conversion calculator

--- programs/temperature_conversion.c
+++ programs/temperature_conversion.c
@@ -16,9 +16,15 @@ int main() {
     unit = toupper(unit);
 
     if (unit == 'C') {
-        printf("\nThe temperature is currently in C\n");
+        printf("\nEnter the temp in Celsius: ");
+        scanf("%f", &temp);
+        temp = (temp * 9 / 5) + 32;
+        printf("\nThe temp in Fahrenheit is: %.1f\n", temp);
     } else if (unit == 'F') {
-        printf("\nThe temperature is currently in F\n");
+        printf("\nEnter the temp in Fahrenheit: ");
+        scanf("%f", &temp);
+        temp = ((temp - 32) * 5) / 9;
+        printf("\nThe temp in Celsius is: %.1f\n", temp);
     } else {
         printf("\n%c is not a valid unit of measurement\n", unit);
     }
