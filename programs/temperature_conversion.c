5227121ca8852db70f84b86abe0fa2e1d96a6ef0
oscar <oscar.zhang9812@gmail.com>
2024-12-18 19:10:07 -0500

    set up basic structure of program inputs and cases

--- programs/temperature_conversion.c
+++ programs/temperature_conversion.c
@@ -1,3 +1,27 @@
 //
 // Created by Oscar Zhang on 12/18/24.
 //
+
+#include <stdio.h>
+#include <ctype.h>
+
+int main() {
+
+    char unit;
+    float temp;
+
+    printf("\nIs the temperature in (F) or (C)? ");
+    scanf("%c", &unit);
+
+    unit = toupper(unit);
+
+    if (unit == 'C') {
+        printf("\nThe temperature is currently in C\n");
+    } else if (unit == 'F') {
+        printf("\nThe temperature is currently in F\n");
+    } else {
+        printf("\n%c is not a valid unit of measurement\n", unit);
+    }
+
+    return 0;
+}
