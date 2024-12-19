129eaa203f7d9076748ff0f1081ad41a5b70f098
oscar <oscar.zhang9812@gmail.com>
2024-12-19 01:51:29 -0500

    created and set up calculator program

---
+++ programs/calculator.c
@@ -0,0 +1,30 @@
+//
+// Created by Oscar Zhang on 12/19/24.
+//
+
+#include <stdio.h>
+
+int main() {
+
+    char operator;
+    double num1;
+    double num2;
+    double result;
+
+    printf("\nEnter an operator (+ - * /): ");
+    scanf("%c", &operator);
+
+    printf("\nEnter number 1: ");
+    scanf("%lf", &num1);
+
+    printf("\nEnter number 2: ");
+    scanf("%lf", &num2);
+
+    switch(operator) {
+
+
+
+    }
+
+    return 0;
+}
