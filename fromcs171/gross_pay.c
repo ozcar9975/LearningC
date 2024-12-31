8e982dec4fef642053ec33c698e4127b79138340
oscar <oscar.zhang9812@gmail.com>
2024-12-30 20:55:54 -0500

    initialized vars

--- fromcs171/gross_pay.c
+++ fromcs171/gross_pay.c
@@ -1,3 +1,19 @@
 //
 // Created by Oscar Zhang on 12/30/24.
 //
+
+#include <stdio.h>
+
+int main() {
+
+    // initialize vars
+    float wage;
+    int hours, weeks;
+
+    // get inputs
+
+
+
+    return 0;
+
+}
