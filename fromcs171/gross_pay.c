a631b5f4c64b6a5b7f2ce055d1021effde3ef7fe
oscar <oscar.zhang9812@gmail.com>
2024-12-30 22:23:11 -0500

    printed net pay info

--- fromcs171/gross_pay.c
+++ fromcs171/gross_pay.c
@@ -25,29 +25,30 @@ int main() {
     // gross pay calculation
     float gross_pay_weekly = wage * hours;
     float gross_pay = gross_pay_weekly * weeks;
+    float take_home = gross_pay;
 
-    printf("Gross Pay Information\n");
+    printf("\nGross Pay Information\n");
     printf("You will make $%'.2f per week.\n", gross_pay_weekly);
-    printf("You will make $%'.2f per year.\n", gross_pay);
+    printf("You will make $%'.2f per year.\n\n", gross_pay);
 
     // deductions
-    float social_security_tax = gross_pay * 0.06;
-    gross_pay -= social_security_tax;
+    float social_security_tax = take_home * 0.06;
+    take_home -= social_security_tax;
 
-    float medicare = gross_pay * 0.02;
-    gross_pay -= medicare;
+    float medicare = take_home * 0.02;
+    take_home -= medicare;
 
-    float wage_tax = gross_pay * 0.12;
-    gross_pay -= wage_tax;
+    float wage_tax = take_home * 0.12;
+    take_home -= wage_tax;
 
-    float state_tax = gross_pay * 0.0307;
-    gross_pay -= state_tax;
+    float state_tax = take_home * 0.0307;
+    take_home -= state_tax;
 
-    float philly_tax = gross_pay * 0.0375;
-    gross_pay -= philly_tax;
+    float philly_tax = take_home * 0.0375;
+    take_home -= philly_tax;
 
     float insurance = 396 * 12;
-    gross_pay -= insurance;
+    take_home -= insurance;
 
     // print results
     printf("Taxes and Deductions\n");
@@ -58,7 +59,17 @@ int main() {
     printf("You will pay $%.2f in Phila Taxes.\n", philly_tax);
     printf("You will pay $%.2f for health insurance.\n\n", insurance);
 
+    // calculations of net pay
+    float weekly_take_home = take_home / weeks;
+    float hourly_take_home = weekly_take_home / hours;
+    float ratio = (take_home / gross_pay) * 100;
 
+    // print results of net pay
+    printf("Net Pay Information\n");
+    printf("Your take home pay will be $%.2f per year.\n", take_home);
+    printf("This is an hourly take home pay of $%.2f.\n", hourly_take_home);
+    printf("This is a weekly take home pay of $%.2f.\n", weekly_take_home);
+    printf("Your Net Pay is %.2f precent of your Gross Pay.\n", ratio);
 
     return 0;
 
